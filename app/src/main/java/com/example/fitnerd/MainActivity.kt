9c05c826f8de5a574d1c03f78603af8c078b6b81
JOAO_PEDRO_TEIXEIRA_REIS <joaopedroteixeirareis@gmail.com>
2025-03-24 09:34:54 -0300

    primeiro commit

--- app/src/main/java/com/example/fitnerd/MainActivity.kt
+++ app/src/main/java/com/example/fitnerd/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.fitnerd
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,11 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnCadastrar = findViewById<Button>(R.id.btn_cadastrar);
+        btnCadastrar.setOnClickListener{
+            Toast.makeText(this, "ola", Toast.LENGTH_LONG).show();
         }
     }
 }
